2616aa1f0dc7f9d68fde78b3c98285858bc46a6f
peter sigurdson <peter@petersigurdson.net>
2019-07-10 08:41:29 -0400

    initial

---
+++ Assignment-3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tp = new TextProcessing();
+            tp.Run();
+        }
+    }
+
+    class TextProcessing
+    { 
+        public void Run()
+        {
+            // Open the names file and access the data
+            using (StreamReader file = new StreamReader("C:/Users/HP/Dropbox/Academic_Desk/ProgramCode/csharpSection12/Assignment-3/Assignment-3/names.txt"))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    counter++;
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+            }
+        }
+    }
+
+}
